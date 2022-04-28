2d8e328f631ad64a1eea7deab9c0ca2a8fe5a7ec
Shreyash-the-Coder <94214458+Shreyash-the-Coder@users.noreply.github.com>
2022-04-28 10:49:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+noseX=0;
+noseY=0;
+
+function preload(){
+    mustache_filter=loadImage('https://i.postimg.cc/ry94tn6t/m.png');
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+ poseNet = ml5.poseNet(video, modelLoaded);
+ poseNet.on('pose', gotPoses);
+}
+function modelLoaded(){
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.x -17;
+        noseY = results[0].pose.nose.y +3;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+        
+    }
+
+}
+
+function draw(){
+    image(video, 0 ,0, 300, 300);
+    image(mustache_filter, noseX, noseY, 35, 35);
+}
+
+function  take_snapshot(){
+    save('myFilterImage.png');
+}
